50cb28f832a5f211d2245fa656eba4df81a3a183
Jiaxin <ishijiaxin@126.com>
2016-10-20 22:55:10 +0800

    model primitives: in progress

---
+++ tests/model/test_base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import pytest
+
+from .context import zhusuan
+from zhusuan.model.base import *
+
+
+class TestStochasticTensor:
+    def test_init(self):
+        pass
+
+    def test_sample(self):
+        pass
+
+    def test_log_p(self):
+        pass
+
+
+class TestStochasticGraph:
+    def test_init(self):
+        pass
+
+    def test_add_stochastic_tensor(self):
+        pass
+
+    def test_get_output(self):
+        pass
--- zhusuan/model/base.py
+++ zhusuan/model/base.py
@@ -6,37 +6,102 @@ from __future__ import print_function
 from __future__ import division
 from collections import OrderedDict
 
+import six
 import tensorflow as tf
 import tensorflow.contrib.graph_editor as ge
 
-from .utils import Context, get_unique_graph, get_backward_tensors
+from .utils import Context, get_backward_tensors
 
 
 class StochasticTensor(object):
-    def __init__(self, inputs):
-        model = Model.get_context()
+    """
+    The :class:`StochasticTensor` class is the base class for various
+    distributions used when building stochastic graphs. It is a wrapper
+    on Tensor instances which enables transparent building of stochastic graphs
+    using Tensorflow primitives.
+
+    :param incomings: A list of Tensors. Parameters needed to specify the
+        distribution.
+    """
+    def __init__(self, incomings):
+        model = StochasticGraph.get_context()
         model.add_stochastic_tensor(self)
         self.value = self.sample()
-        self.inputs = inputs
+        self.incomings = incomings
 
-    def sample(self):
+    def sample(self, **kwargs):
+        """
+        Get samples from the distribution.
+
+        :return: A Tensor.
+        """
         raise NotImplementedError()
 
-    def logpdf(self, given):
+    def log_p(self, given, inputs):
+        """
+        Compute log probability density (mass) function at `given` values,
+        provided with parameters `inputs`.
+
+        :param given: A Tensor. The value at which to evaluate log probability
+            density (mass) function.
+        :param inputs: A list of Tensors. Parameters needed to specify the
+            distribution.
+
+        :return: A Tensor. The log probability density (mass) evaluated.
+        """
         raise NotImplementedError()
 
 
-class Model(Context):
+class StochasticGraph(Context):
     """
-    A context class supporting model construction in ZhuSuan.
+    A context class supporting model construction in ZhuSuan as stochastic
+    graphs.
     """
     def __init__(self):
         self.stochastic_tensors = OrderedDict()
 
     def add_stochastic_tensor(self, s_tensor):
+        """
+        Add a stochastic tensor to the graph. This is the function called when
+        a stochastic tensor is created in the context.
+
+        :param s_tensor: A :class:`StochasticTensor` instance.
+        """
         self.stochastic_tensors[s_tensor.value] = s_tensor
 
     def get_output(self, tensor_or_tensors, inputs=None):
+        """
+        Compute the outputs and log probability density/mass values
+        at one or more nodes of the stochastic graph.
+
+        By default, the stochastic graph samples Tensors at stochastic nodes
+        and propagates them through deterministic nodes (nodes constructed by
+        Tensorflow operations) to get outputs.
+        Optionally, one can feed given values at any nodes to propagate
+        through the graph instead of using original Tensors. This is intended
+        for an "observe" action in stochastic graphs, which is a common
+        scene when building probabilistic graphical models.
+
+        :param tensor_or_tensors: A Tensor or a list of Tensors. for which to
+            get the output Tensors by building Tensorflow computation graphs
+            consisting of forward propagation and sampling.
+        :param inputs: A dictionary or None. Any nodes in the current graph
+            can be mapped to a Tensor to use instead of its regular output.
+
+        :return: A tuple or list of tuples. The outputs and log probability
+            density/mass values at `tensor_or_tensors`.
+
+            For deterministic nodes (d):
+            If given replacements through the `inputs` argument (inputs[d]),
+            the returned tuple is (inputs[d], None).
+            If not, the returned tuple is (output_of_d, None).
+
+            For stochastic nodes (r):
+            If given replacements through the `inputs` argument (inputs[r]),
+            the returned tuple is (inputs[r], log_p_at_inputs[r])
+            If not, the returned tuple is (samples_of_r,
+            log_p_at_samples_of_r).
+        """
         requested_tensors = tensor_or_tensors
         if not isinstance(tensor_or_tensors, (list, tuple)):
             requested_tensors = [tensor_or_tensors]
@@ -51,10 +116,9 @@ class Model(Context):
                 if tensor in self.stochastic_tensors:
                     s_tensor = self.stochastic_tensors[tensor]
                     if tensor in inputs:
-                        output[1] = s_tensor.logpdf(inputs[tensor],
-                                                    s_tensor.inputs)
+                        output[1] = s_tensor.log_p(inputs[tensor])
                     else:
-                        output[1] = s_tensor.logpdf(output[0], s_tensor.inputs)
+                        output[1] = s_tensor.log_p(output[0])
                 ret.append(output)
         else:
             # inputs are observed
@@ -63,7 +127,7 @@ class Model(Context):
 
             inputs = dict([(k.value, v)
                            if isinstance(k, StochasticTensor) else (k, v)
-                           for k, v in inputs])
+                           for k, v in six.iteritems(inputs)])
 
             def _whether_treat_as_inputs(tensor):
                 """
@@ -83,46 +147,60 @@ class Model(Context):
                     return True
 
             treat_as_inputs = filter(_whether_treat_as_inputs, inputs.keys())
-            graph = get_unique_graph(requested_tensors)
             all_tensors = get_backward_tensors(requested_tensors,
                                                treat_as_inputs)
 
-            # copy each op's surrounding subgraph by topological order.
+            # copy each op by topological order.
             copied_tensors = {}
             copied_ops = set()
-            copy_info = {}
+
+            def _replace_t_with_replacement_handler(info, t):
+                if t in inputs:
+                    return inputs[t]
+                elif t in copied_tensors:
+                    return copied_tensors[t]
+                else:
+                    return ge.keep_t_if_possible_handler(info, t)
+
             for tensor in all_tensors:
-                # TODO: control dependencies, variable reuse
                 if tensor.op not in copied_ops:
-                    op_inputs = tensor.op.inputs[:]
-                    op_outputs = tensor.op.outputs[:]
-                    replacement_ts = {}
-                    for op_input in op_inputs:
-                        if op_input in inputs:
-                            replacement_ts[op_input] = inputs[op_input]
-                        elif op_input in copied_tensors:
-                            replacement_ts[op_input] = copied_tensors[op_input]
                     sgv = ge.make_view([tensor.op])
-                    copied_sgv, info = ge.copy_with_input_replacements(
-                        sgv, replacement_ts)
+                    copier = ge.Transformer()
+                    copier.transform_external_input_handler = \
+                        _replace_t_with_replacement_handler
+                    copier.transform_control_input_handler = \
+                        _replace_t_with_replacement_handler
+                    # not changing scope for now
+                    copied_sgv, info = copier(sgv, sgv.graph, "", "",
+                                              reuse_dst_scope=True)
                     copied_ops.add(tensor.op)
+                    op_outputs = tensor.op.outputs[:]
                     for output in op_outputs:
                         copied_tensors[output] = info.transformed(output)
-                        copy_info[output] = info
+
+            def _get_output_tensor(t):
+                """
+                Get corresponding output of a tensor.
+                """
+                if t in inputs:
+                    return inputs[t]
+                elif tensor in copied_tensors:
+                    return copied_tensors[t]
+                else:
+                    return t
 
             # compute log probability density for each requested tensor
             ret = []
             for tensor in requested_tensors:
-                output = [copied_tensors[tensor], None]
+                output = [_get_output_tensor(tensor), None]
                 if tensor in self.stochastic_tensors:
                     s_tensor = self.stochastic_tensors[tensor]
-                    copied_inputs = [copied_tensors.get(t, t)
-                                     for t in s_tensor.inputs]
-                    if tensor in inputs:
-                        output[1] = s_tensor.logpdf(inputs[tensor],
-                                                    copied_inputs)
-                    else:
-                        output[1] = s_tensor.logpdf(output[0], copied_inputs)
+                    dist_inputs = list(map(_get_output_tensor,
+                                           s_tensor.inputs))
+                    output[1] = s_tensor.log_p(output[0], dist_inputs)
                 ret.append(output)
 
-        return ret
+        if isinstance(tensor_or_tensors, (tuple, list)):
+            return ret
+        else:
+            return ret[0]
