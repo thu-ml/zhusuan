556d8a2b627492e8e720ade5510c9c0571771d42
yihong <wmyw96@126.com>
2017-03-26 15:02:03 +0800

    clean code

--- zhusuan/__init__.py
+++ zhusuan/__init__.py
@@ -4,7 +4,6 @@
 from . import distributions
 from .model import *
 from .variational import *
-from .transform import *
 from .hmc import *
 from .evaluation import *
 from .utils import log_mean_exp
