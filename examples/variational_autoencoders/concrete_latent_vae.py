94cf2fa7e0415f94814d154cb03d04a3dbeb80ed
Yuhao Zhou <miskcoo@gmail.com>
2017-07-14 17:56:58 +0800

    concrete

--- examples/variational_autoencoders/concrete_latent_vae.py
+++ examples/variational_autoencoders/concrete_latent_vae.py
@@ -18,30 +18,40 @@ from examples.utils import dataset, save_image_collections
 
 
 temperature_prior = 0.5
-temperature_posterior = 0.66
+temperature_posterior = 0.666
+
+
+def stacked_logits(logits):
+    logits0 = tf.log_sigmoid(-logits)
+    logits1 = tf.log_sigmoid(logits)
+    return tf.stack([logits0, logits1], axis=-1)
 
 
 @zs.reuse('model')
-def vae(observed, n, n_x, n_z, n_particles, is_training, relaxed):
+def vae(observed, n, n_x, n_z, n_k, n_particles, is_training, relaxed):
     with zs.BayesianNet(observed=observed) as model:
         normalizer_params = {'is_training': is_training,
                              'updates_collections': None}
+        z_logits = tf.get_variable('z_logits', [n_z * n_k])
+        z_logits = tf.tile(tf.expand_dims(z_logits, 0), [n, 1])
+        z_stacked_logits = tf.reshape(z_logits, [-1, n, n_z, n_k])
         if relaxed:
-            z = zs.ExpConcrete('z', temperature_prior, tf.zeros([n, n_z, 2]),
+            z = zs.ExpConcrete('z', temperature_prior, z_stacked_logits,
                                n_samples=n_particles, group_event_ndims=1)
-            unpacked_z = tf.unstack(z, axis=-1)
-            z = tf.exp(unpacked_z[1])
+            z = tf.exp(tf.reshape(z, [-1, n, n_z * n_k]))
         else:
-            z = zs.Bernoulli('z', tf.zeros([n, n_z]),
+            z = zs.OnehotCategorical('z', z_stacked_logits,
                              n_samples=n_particles, group_event_ndims=1)
-            z = tf.to_float(z)
+            z = tf.to_float(tf.reshape(z, [-1, n, n_z * n_k]))
         #lx_z = layers.fully_connected(z, 500, activation_fn=tf.tanh)
         #lx_z = layers.fully_connected(lx_z, 500, activation_fn=tf.tanh)
         lx_z = layers.fully_connected(
-            z, 500, normalizer_fn=layers.batch_norm,
+            z, 500, activation_fn=tf.tanh,
+            normalizer_fn=layers.batch_norm,
             normalizer_params=normalizer_params)
         lx_z = layers.fully_connected(
-            lx_z, 500, normalizer_fn=layers.batch_norm,
+            lx_z, 500, activation_fn=tf.tanh,
+            normalizer_fn=layers.batch_norm,
             normalizer_params=normalizer_params)
         x_logits = layers.fully_connected(lx_z, n_x, activation_fn=None)
         x = zs.Bernoulli('x', x_logits, group_event_ndims=1)
@@ -49,7 +59,7 @@ def vae(observed, n, n_x, n_z, n_particles, is_training, relaxed):
 
 
 @zs.reuse('variational')
-def q_net(observed, x, n_z, n_particles, is_training, relaxed):
+def q_net(observed, x, n_z, n_k, n_particles, is_training, relaxed):
     with zs.BayesianNet(observed=observed) as variational:
         normalizer_params = {'is_training': is_training,
                              'updates_collections': None}
@@ -57,20 +67,20 @@ def q_net(observed, x, n_z, n_particles, is_training, relaxed):
         #    tf.to_float(x), 500, activation_fn=tf.tanh)
         #lz_x = layers.fully_connected(lz_x, 500, activation_fn=tf.tanh)
         lz_x = layers.fully_connected(
-            tf.to_float(x), 500, normalizer_fn=layers.batch_norm,
+            tf.to_float(x), 500, activation_fn=tf.tanh,
+            normalizer_fn=layers.batch_norm,
             normalizer_params=normalizer_params)
         lz_x = layers.fully_connected(
-            lz_x, 500, normalizer_fn=layers.batch_norm,
+            lz_x, 500, activation_fn=tf.tanh,
+            normalizer_fn=layers.batch_norm,
             normalizer_params=normalizer_params)
-        z_logits = layers.fully_connected(lz_x, n_z, activation_fn=None)
+        z_logits = layers.fully_connected(lz_x, n_z * n_k, activation_fn=None)
+        z_stacked_logits = tf.reshape(z_logits, [-1, n, n_z, n_k])
         if relaxed:
-            z_logits0 = tf.log_sigmoid(-z_logits)
-            z_logits1 = tf.log_sigmoid(z_logits)
-            z_packed_logits = tf.stack([z_logits0, z_logits1], axis=-1)
-            z = zs.ExpConcrete('z', temperature_posterior, z_packed_logits,
+            z = zs.ExpConcrete('z', temperature_posterior, z_stacked_logits,
                                n_samples=n_particles, group_event_ndims=1)
         else:
-            z = zs.Bernoulli('z', z_logits,
+            z = zs.OnehotCategorical('z', z_stacked_logits,
                              n_samples=n_particles, group_event_ndims=1)
     return variational
 
@@ -87,20 +97,20 @@ if __name__ == '__main__':
     x_test = np.random.binomial(1, x_test, size=x_test.shape).astype('float32')
 
     # Define parameters
-    n_z = 40
+    n_z, n_k = 100, 2   # number of latent variables, categories
     n_x = x_train.shape[1]
-    lb_samples = 10
+    lb_samples = 5
     ll_samples = 500
-    epochs = 3000
-    batch_size = 64
+    epochs = 10000
+    batch_size = 100
     iters = x_train.shape[0] // batch_size
     learning_rate = 0.001
     anneal_lr_freq = 200
-    anneal_lr_rate = 0.75
+    anneal_lr_rate = 1.0
     test_freq = 50
     test_batch_size = 400
     test_iters = x_test.shape[0] // test_batch_size
-    save_freq = 100
+    save_freq = 20
     result_path = "results/vae"
 
     # Build the computation graph
@@ -115,12 +125,12 @@ if __name__ == '__main__':
 
     def lower_bound_and_log_likelihood(relaxed):
         def log_joint(observed):
-            model = vae(observed, n, n_x, n_z, n_particles,
-                        is_training, relaxed)
+            model = vae(observed, n, n_x, n_z, n_k,
+                        n_particles, is_training, relaxed)
             log_pz, log_px_z = model.local_log_prob(['z', 'x'])
             return log_pz + log_px_z
 
-        variational = q_net({}, x, n_z, n_particles, is_training, relaxed)
+        variational = q_net({}, x, n_z, n_k, n_particles, is_training, relaxed)
         qz_samples, log_qz = variational.query('z', outputs=True,
                                                local_log_prob=True)
         lower_bound = tf.reduce_mean(zs.sgvb(
@@ -145,7 +155,7 @@ if __name__ == '__main__':
 
     # Generate images
     n_gen = 100
-    model = vae({}, n_gen, n_x, n_z, 1, is_training, False)
+    model = vae({}, n_gen, n_x, n_z, n_k, 1, is_training, False)
     x_logits = model._stochastic_tensors['x'].distribution.logits
     x_gen = tf.reshape(tf.sigmoid(x_logits), [-1, 28, 28, 1])
 
@@ -195,7 +205,7 @@ if __name__ == '__main__':
                                           (t + 1) * test_batch_size]
                     test_lb = sess.run(lower_bound,
                                        feed_dict={x: test_x_batch,
-                                                  n_particles: lb_samples,
+                                                  n_particles: ll_samples,
                                                   is_training: False})
                     test_ll = sess.run(is_log_likelihood,
                                        feed_dict={x: test_x_batch,
@@ -217,7 +227,7 @@ if __name__ == '__main__':
                     os.makedirs(os.path.dirname(save_path))
                 saver.save(sess, save_path)
 
-                images = sess.run(x_gen)
+                images = sess.run(x_gen, feed_dict={is_training: False})
                 name = "results/vae/vae.epoch.{}.png".format(epoch)
                 save_image_collections(images, name)
                 print('Done')
