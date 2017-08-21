9dd5dd78589b823725248168c2eeaeae6cfdaa7a
csy <csy530216@126.com>
2017-08-21 23:32:25 +0800

    Add AIS evaluation for lntm model.

--- examples/topic_models/lntm_mcem.py
+++ examples/topic_models/lntm_mcem.py
@@ -27,11 +27,15 @@ log_delta = 10.0
 
 
 @zs.reuse('model')
-def lntm(observed, D, K, V, eta_mean, eta_logstd):
+def lntm(observed, n_chains, D, K, V, eta_mean, eta_logstd):
     with zs.BayesianNet(observed=observed) as model:
         eta = zs.Normal('eta',
-                        tf.tile(tf.expand_dims(eta_mean, 0), [D, 1]),
-                        logstd=tf.tile(tf.expand_dims(eta_logstd, 0), [D, 1]),
+                        tf.tile(tf.expand_dims(
+                            tf.tile(tf.expand_dims(eta_mean, 0), [D, 1]),
+                            0), [n_chains, 1, 1]),
+                        logstd=tf.tile(tf.expand_dims(
+                            tf.tile(tf.expand_dims(eta_logstd, 0), [D, 1]),
+                            0), [n_chains, 1, 1]),
                         group_event_ndims=1)
         beta = zs.Normal('beta', tf.zeros([K, V]),
                          logstd=tf.ones([K, V]) * log_delta,
@@ -53,6 +57,8 @@ if __name__ == "__main__":
     D = 100
     K = 100
     V = X_train.shape[1]
+    n_chains = 1
+
     num_e_steps = 5
     hmc = zs.HMC(step_size=1e-3, n_leapfrogs=20, adapt_step_size=True,
                  target_acceptance_rate=0.6)
@@ -67,7 +73,7 @@ if __name__ == "__main__":
 
     T = np.sum(X_train)
     iters = X_train.shape[0] // D
-    Eta = np.zeros((X_train.shape[0], K), dtype=np.float32)
+    Eta = np.zeros((n_chains, X_train.shape[0], K), dtype=np.float32)
     Eta_mean = np.zeros(K, dtype=np.float32)
     Eta_logstd = np.zeros(K, dtype=np.float32)
 
@@ -75,26 +81,30 @@ if __name__ == "__main__":
     x = tf.placeholder(tf.float32, shape=[D, V], name='x')
     eta_mean = tf.placeholder(tf.float32, shape=[K], name='eta_mean')
     eta_logstd = tf.placeholder(tf.float32, shape=[K], name='eta_logstd')
-    eta = tf.Variable(tf.zeros([D, K]), name='eta')
-    eta_ph = tf.placeholder(tf.float32, shape=[D, K], name='eta_ph')
+    eta = tf.Variable(tf.zeros([n_chains, D, K]), name='eta')
+    eta_ph = tf.placeholder(tf.float32, shape=[n_chains, D, K], name='eta_ph')
     beta = tf.Variable(tf.zeros([K, V]), name='beta')
     phi = tf.nn.softmax(beta)
-    init_eta = tf.assign(eta, tf.zeros([D, K]))
     init_eta_ph = tf.assign(eta, eta_ph)
 
     def joint_obj(observed):
-        model = lntm(observed, D, K, V, eta_mean, eta_logstd)
-        # [D], [K], [K]
+        model = lntm(observed, n_chains, D, K, V, eta_mean, eta_logstd)
+
         log_p_eta, log_p_beta = \
             model.local_log_prob(['eta', 'beta'])
 
         theta = tf.nn.softmax(observed['eta'])
         phi = tf.nn.softmax(observed['beta'])
-        pred = tf.matmul(theta, phi)
+        tiled_phi = tf.tile(tf.expand_dims(phi, 0), [n_chains, 1, 1])
+        pred = tf.matmul(theta, tiled_phi)
 
-        # [D]
-        log_px = tf.reduce_sum(observed['x'] * tf.log(pred), -1)
+        tiled_x = tf.tile(tf.expand_dims(observed['x'], 0), [n_chains, 1, 1])
 
+        log_px = tf.reduce_sum(tiled_x * tf.log(pred), -1)
+
+        # Shape:
+        # log_p_eta, log_px: [n_chains, D]
+        # log_p_beta: [K]
         return log_p_eta, log_p_beta, log_px
 
     def e_obj(observed):
@@ -102,7 +112,7 @@ if __name__ == "__main__":
         return log_p_eta + log_px
 
     lp_eta, lp_beta, lp_x = joint_obj({'x': x, 'eta': eta, 'beta': beta})
-    log_likelihood = tf.reduce_sum(lp_x)
+    log_likelihood = tf.reduce_sum(tf.reduce_mean(lp_x, axis=0), axis=0)
     log_joint = tf.reduce_sum(lp_beta) + log_likelihood
     sample_op, hmc_info = hmc.sample(
         e_obj, observed={'x': x, 'beta': beta}, latent={'eta': eta})
@@ -115,6 +125,51 @@ if __name__ == "__main__":
     for i in params:
         print(i.name, i.get_shape())
 
+    # Below is the evaluation part.
+    # Variables whose name starts with '_' is only used in the evaluation part,
+    # to be distinguished from those variables used in the training part above.
+
+    _D = X_test.shape[0]
+    _T = np.sum(X_test)
+    _n_chains = 25
+    _n_temperatures = 1000
+
+    _x = tf.placeholder(tf.float32, shape=[_D, V], name='x')
+    _eta = tf.Variable(tf.zeros([_n_chains, _D, K]), name='eta')
+
+    def _log_prior(observed):
+        _model = lntm(observed, _n_chains, _D, K, V, eta_mean, eta_logstd)
+        return _model.local_log_prob('eta')
+
+    def _log_joint(observed):
+        _model = lntm(observed, _n_chains, _D, K, V, eta_mean, eta_logstd)
+        _log_prior = _model.local_log_prob('eta')
+
+        _theta = tf.nn.softmax(observed['eta'])
+        phi = tf.nn.softmax(observed['beta'])
+        tiled_phi = tf.tile(tf.expand_dims(phi, 0), [_n_chains, 1, 1])
+        _pred = tf.matmul(_theta, tiled_phi)
+
+        tiled_x = tf.tile(tf.expand_dims(observed['x'], 0), [_n_chains, 1, 1])
+
+        _log_px = tf.reduce_sum(tiled_x * tf.log(_pred), -1)
+
+        return _log_prior + _log_px
+
+    _prior_samples = {'eta': lntm({}, _n_chains, _D, K, V,
+                      eta_mean, eta_logstd).outputs('eta')}
+
+    _hmc = zs.HMC(step_size=0.01, n_leapfrogs=20, adapt_step_size=True,
+                  target_acceptance_rate=0.6)
+
+    _ais = zs.evaluation.AIS(_log_prior, _log_joint, _prior_samples, _hmc,
+                             observed={'x': _x, 'beta': beta},
+                             latent={'eta': _eta},
+                             n_chains=_n_chains,
+                             n_temperatures=_n_temperatures)
+
+    # -------------------
+
     # Run the inference
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -125,12 +180,12 @@ if __name__ == "__main__":
             perm = list(range(X_train.shape[0]))
             np.random.shuffle(perm)
             X_train = X_train[perm, :]
-            Eta = Eta[perm, :]
+            Eta = Eta[:, perm, :]
             lls = []
             accs = []
             for t in range(iters):
                 x_batch = X_train[t * D: (t + 1) * D]
-                old_eta = Eta[t * D:(t + 1) * D, :]
+                old_eta = Eta[:, t * D:(t + 1) * D, :]
 
                 # E step
                 sess.run(init_eta_ph, feed_dict={eta_ph: old_eta})
@@ -144,7 +199,7 @@ if __name__ == "__main__":
                     accs.append(acc)
                     # Store eta for the persistent chain
                     if j + 1 == num_e_steps:
-                        Eta[t * D:(t + 1) * D, :] = new_eta
+                        Eta[:, t * D:(t + 1) * D, :] = new_eta
 
                 # M step
                 _, ll = sess.run(
@@ -157,8 +212,8 @@ if __name__ == "__main__":
                 lls.append(ll)
 
             # Update hyper-parameters
-            Eta_mean = np.mean(Eta, axis=0)
-            Eta_logstd = np.log(np.std(Eta, axis=0) + 1e-6)
+            Eta_mean = np.mean(Eta, axis=(0, 1))
+            Eta_logstd = np.log(np.std(Eta, axis=(0, 1)) + 1e-6)
 
             time_epoch += time.time()
             print('Epoch {} ({:.1f}s): Perplexity = {:.2f}, acc = {:.3f}, '
@@ -166,6 +221,22 @@ if __name__ == "__main__":
                   .format(epoch, time_epoch, np.exp(-np.sum(lls) / T),
                           np.mean(accs), np.mean(Eta_mean),
                           np.mean(Eta_logstd)))
+
+        # Run AIS
+        print('Start evaluation! {} temperatures, {} chains'
+              .format(_n_temperatures, _n_chains))
+        time_ais = -time.time()
+
+        ll_lb = _ais.run(sess, feed_dict={_x: X_test,
+                                          eta_mean: Eta_mean,
+                                          eta_logstd: Eta_logstd})
+
+        time_ais += time.time()
+
+        print('>> Test perplexity (AIS) ({:.1f}s)\n'
+              '>> loglikelihood lower bound = {}\n'
+              '>> perplexity upper bound = {}'
+              .format(time_ais, ll_lb, np.exp(-ll_lb * _D / _T)))
 
         # Output topics
         p = sess.run(phi)
--- zhusuan/evaluation.py
+++ zhusuan/evaluation.py
@@ -5,9 +5,10 @@ from __future__ import absolute_import
 from __future__ import division
 
 import six
-from six.moves import zip, map
+from six.moves import zip, map, range
 import tensorflow as tf
 import numpy as np
+from scipy.misc import logsumexp
 
 from zhusuan.utils import log_mean_exp, merge_dicts
 
@@ -48,23 +49,21 @@ def is_loglikelihood(log_joint, observed, latent, axis=None):
     return log_w
 
 
-class BDMC:
+class AIS:
     """
-    Marginal log likelihood estimates using Bidirectional Monte Carlo (Grosse,
-    2015), which estimates a stochastic lower bound and a stochastic upper
-    bound of the marginal log likelihood using annealed importance sampling
-    (AIS).
+    Estimates a stochastic lower bound of the marginal log likelihood
+    using annealed importance sampling(AIS).
     """
     def __init__(self, log_prior, log_joint, prior_sampler,
                  hmc, observed, latent, n_chains, n_temperatures):
-        # Shape of latent: [chain_axis * num_data, data dims]
+        # Shape of latent: [chain_axis, num_data, data dims]
         # Construct the tempered objective
         self.prior_sampler = prior_sampler
         self.latent = latent
         self.n_chains = n_chains
         self.n_temperatures = n_temperatures
 
-        with tf.name_scope("BDMC"):
+        with tf.name_scope("AIS"):
             self.temperature = tf.placeholder(tf.float32, shape=[],
                                               name="temperature")
 
@@ -80,6 +79,9 @@ class BDMC:
                                 for z, z_s in zip(latent.values(),
                                                   self.prior_sampler.values())]
 
+    def map_t(self, t):
+        return 1. / (1. + np.exp(-4*(2*t / self.n_temperatures - 1)))
+
     def run(self, sess, feed_dict):
         # Draw a sample from the prior
         sess.run(self.init_latent, feed_dict=feed_dict)
@@ -88,43 +90,28 @@ class BDMC:
                                      feed_dict, {self.temperature: 0}))
         log_weights = -prior_density
 
-        # Forward AIS
         for num_t in range(self.n_temperatures):
-            current_temperature = 1.0 / self.n_temperatures * (num_t + 1)
-            new_feed_dict = feed_dict.copy()
-            new_feed_dict[self.temperature] = current_temperature
-            _, old_log_p, new_log_p = sess.run(
-                [self.sample_op, self.hmc_info.orig_log_prob,
-                 self.hmc_info.log_prob], feed_dict=new_feed_dict)
-            if num_t + 1 < self.n_temperatures:
-                log_weights += old_log_p - new_log_p
-            else:
-                log_weights += old_log_p
+            # current_temperature = 1.0 / self.n_temperatures * (num_t + 1)
+            current_temperature = (self.map_t(num_t+1) - self.map_t(0)) \
+                                  / (self.map_t(self.n_temperatures) -
+                                     self.map_t(0))
 
-        ll_lb = np.mean(self.get_lower_bound(log_weights))
-
-        # Backward AIS
-        log_weights = -new_log_p
-        for num_t in range(self.n_temperatures):
-            current_temperature = 1.0 - 1.0 / self.n_temperatures * (num_t + 1)
-            _, old_log_p, new_log_p = sess.run(
+            _, old_log_p, new_log_p, acc = sess.run(
                 [self.sample_op, self.hmc_info.orig_log_prob,
-                 self.hmc_info.log_prob],
+                 self.hmc_info.log_prob, self.hmc_info.acceptance_rate],
                 feed_dict=merge_dicts(feed_dict,
                                       {self.temperature: current_temperature}))
+
             if num_t + 1 < self.n_temperatures:
                 log_weights += old_log_p - new_log_p
             else:
                 log_weights += old_log_p
 
-        ll_ub = -np.mean(self.get_lower_bound(log_weights))
+            if (num_t % 10 == 9):
+                print('Finished steps: {}, Temperature = {:.4f}, acc = {:.4f}'
+                      .format(num_t+1, current_temperature, np.mean(acc)))
 
-        return ll_lb, ll_ub
+        return np.mean(self.get_lower_bound(log_weights))
 
     def get_lower_bound(self, log_weights):
-        max_log_weights = np.max(log_weights, axis=0)
-        offset_log_weights = np.sum(np.exp(log_weights - max_log_weights),
-                                    axis=0)
-        log_weights = np.log(offset_log_weights) + max_log_weights - \
-            np.log(self.n_chains)
-        return log_weights
+        return logsumexp(log_weights, axis=0) - np.log(self.n_chains)
