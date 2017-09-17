85ae4a75767f48ed61a5aa50d22ce8001b0ac414
Jiaxin <ishijiaxin@126.com>
2017-09-17 16:43:22 +0800

    Simplify the sbn examples.

--- examples/sigmoid_belief_nets/sbn_vimco.py
+++ examples/sigmoid_belief_nets/sbn_vimco.py
@@ -18,35 +18,34 @@ from examples.utils import dataset
 
 
 @zs.reuse('model')
-def sbn(observed, n, n_x, n_h, n_particles, is_training):
+def sbn(observed, n, n_x, n_h, n_particles):
     with zs.BayesianNet(observed=observed) as model:
         h3_logits = tf.zeros([n, n_h])
         h3 = zs.Bernoulli('h3', h3_logits, n_samples=n_particles,
-                          group_event_ndims=1)
-        h2_logits = layers.fully_connected(
-            tf.to_float(h3), n_h, activation_fn=None)
-        h2 = zs.Bernoulli('h2', h2_logits, group_event_ndims=1)
-        h1_logits = layers.fully_connected(
-            tf.to_float(h2), n_h, activation_fn=None)
-        h1 = zs.Bernoulli('h1', h1_logits, group_event_ndims=1)
-        x_logits = layers.fully_connected(
-            tf.to_float(h1), n_x, activation_fn=None)
+                          group_event_ndims=1, dtype=tf.float32)
+        h2_logits = layers.fully_connected(h3, n_h, activation_fn=None)
+        h2 = zs.Bernoulli('h2', h2_logits, group_event_ndims=1,
+                          dtype=tf.float32)
+        h1_logits = layers.fully_connected(h2, n_h, activation_fn=None)
+        h1 = zs.Bernoulli('h1', h1_logits, group_event_ndims=1,
+                          dtype=tf.float32)
+        x_logits = layers.fully_connected(h1, n_x, activation_fn=None)
         x = zs.Bernoulli('x', x_logits, group_event_ndims=1)
     return model
 
 
-def q_net(x, n_h, n_particles, is_training):
+def q_net(x, n_h, n_particles):
     with zs.BayesianNet() as variational:
         h1_logits = layers.fully_connected(
             tf.to_float(x), n_h, activation_fn=None)
         h1 = zs.Bernoulli('h1', h1_logits, n_samples=n_particles,
-                          group_event_ndims=1)
-        h2_logits = layers.fully_connected(
-            tf.to_float(h1), n_h, activation_fn=None)
-        h2 = zs.Bernoulli('h2', h2_logits, group_event_ndims=1)
-        h3_logits = layers.fully_connected(
-            tf.to_float(h2), n_h, activation_fn=None)
-        h3 = zs.Bernoulli('h3', h3_logits, group_event_ndims=1)
+                          group_event_ndims=1, dtype=tf.float32)
+        h2_logits = layers.fully_connected(h1, n_h, activation_fn=None)
+        h2 = zs.Bernoulli('h2', h2_logits, group_event_ndims=1,
+                          dtype=tf.float32)
+        h3_logits = layers.fully_connected(h2, n_h, activation_fn=None)
+        h3 = zs.Bernoulli('h3', h3_logits, group_event_ndims=1,
+                          dtype=tf.float32)
     return variational
 
 
@@ -79,7 +78,6 @@ if __name__ == "__main__":
     anneal_lr_rate = 0.75
 
     # Build the computation graph
-    is_training = tf.placeholder(tf.bool, shape=[], name='is_training')
     n_particles = tf.placeholder(tf.int32, shape=[], name='n_particles')
 
     x_orig = tf.placeholder(tf.float32, shape=[None, n_x], name='x')
@@ -88,42 +86,26 @@ if __name__ == "__main__":
     x = tf.placeholder(tf.int32, shape=[None, n_x], name='x')
     x_obs = tf.tile(tf.expand_dims(x, 0), [n_particles, 1, 1])
     n = tf.shape(x)[0]
+    h_names = ['h' + str(i + 1) for i in range(3)]
 
     def log_joint(observed):
-        model = sbn(observed, n, n_x, n_h, n_particles, is_training)
+        model = sbn(observed, n, n_x, n_h, n_particles)
         log_ph3, log_ph2_h3, log_ph1_h2, log_px_h1 = model.local_log_prob(
             ['h3', 'h2', 'h1', 'x'])
         return log_ph3 + log_ph2_h3 + log_ph1_h2 + log_px_h1
 
-    variational = q_net(x, n_h, n_particles, is_training)
-    qh3_samples, log_qh3 = variational.query('h3', outputs=True,
-                                             local_log_prob=True)
-    qh2_samples, log_qh2 = variational.query('h2', outputs=True,
-                                             local_log_prob=True)
-    qh1_samples, log_qh1 = variational.query('h1', outputs=True,
-                                             local_log_prob=True)
+    variational = q_net(x, n_h, n_particles)
+    qh_outputs = variational.query(h_names, outputs=True, local_log_prob=True)
+    latent = dict(zip(h_names, qh_outputs))
 
     lower_bound = zs.variational.importance_weighted_objective(
-        log_joint, observed={'x': x_obs},
-        latent={'h3': [qh3_samples, log_qh3],
-                'h2': [qh2_samples, log_qh2],
-                'h1': [qh1_samples, log_qh1]}, axis=0)
+        log_joint, observed={'x': x_obs}, latent=latent, axis=0)
     cost = tf.reduce_mean(lower_bound.vimco())
     lower_bound = tf.reduce_mean(lower_bound)
-
-    log_likelihood = tf.reduce_mean(
-        zs.is_loglikelihood(log_joint, {'x': x_obs},
-                            {'h3': [qh3_samples, log_qh3],
-                             'h2': [qh2_samples, log_qh2],
-                             'h1': [qh1_samples, log_qh1]}, axis=0))
 
     learning_rate_ph = tf.placeholder(tf.float32, shape=[], name='lr')
     optimizer = tf.train.AdamOptimizer(learning_rate_ph, epsilon=1e-4)
     infer_op = optimizer.minimize(cost)
-
-    params = tf.trainable_variables()
-    for i in params:
-        print(i.name, i.get_shape())
 
     # Run the inference
     with tf.Session() as sess:
@@ -140,8 +122,7 @@ if __name__ == "__main__":
                 _, lb = sess.run([infer_op, lower_bound],
                                  feed_dict={x: x_batch_bin,
                                             learning_rate_ph: learning_rate,
-                                            n_particles: lb_samples,
-                                            is_training: True})
+                                            n_particles: lb_samples})
                 lbs.append(lb)
             time_epoch += time.time()
             print('Epoch {} ({:.1f}s): Lower bound = {}'.format(
@@ -155,12 +136,10 @@ if __name__ == "__main__":
                         t * test_batch_size: (t + 1) * test_batch_size]
                     test_lb = sess.run(lower_bound,
                                        feed_dict={x: test_x_batch,
-                                                  n_particles: lb_samples,
-                                                  is_training: False})
-                    test_ll = sess.run(log_likelihood,
+                                                  n_particles: lb_samples})
+                    test_ll = sess.run(lower_bound,
                                        feed_dict={x: test_x_batch,
-                                                  n_particles: ll_samples,
-                                                  is_training: False})
+                                                  n_particles: ll_samples})
                     test_lbs.append(test_lb)
                     test_lls.append(test_ll)
                 time_test += time.time()
