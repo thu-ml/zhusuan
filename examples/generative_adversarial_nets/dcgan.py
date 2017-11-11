6be81289d9bc46657a1b14ded440c8160721a464
Jiaxin <ishijiaxin@126.com>
2017-11-11 11:34:51 +0800

    More examples from contrib.layers to tf.layers.

--- examples/generative_adversarial_nets/dcgan.py
+++ examples/generative_adversarial_nets/dcgan.py
@@ -10,7 +10,6 @@ import time
 import numpy as np
 import tensorflow as tf
 from six.moves import range
-from tensorflow.contrib import layers
 import zhusuan as zs
 
 from examples import conf
@@ -21,53 +20,54 @@ from examples.utils.multi_gpu import FLAGS
 @zs.reuse('generator')
 def generator(observed, n, n_z, is_training):
     with zs.BayesianNet(observed=observed) as generator:
-        normalizer_params = {'is_training': is_training,
-                             'updates_collections': None}
         ngf = 64
         z_min = -tf.ones([n, n_z])
         z_max = tf.ones([n, n_z])
         z = zs.Uniform('z', z_min, z_max)
-        lx_z = layers.fully_connected(z, num_outputs=ngf*8*4*4,
-                                      normalizer_fn=layers.batch_norm,
-                                      normalizer_params=normalizer_params)
-        lx_z = tf.reshape(lx_z, [-1, 4, 4, ngf*8])
-        lx_z = layers.conv2d_transpose(lx_z, ngf*4, 5, stride=2,
-                                       normalizer_fn=layers.batch_norm,
-                                       normalizer_params=normalizer_params)
-        lx_z = layers.conv2d_transpose(lx_z, ngf*2, 5, stride=2,
-                                       normalizer_fn=layers.batch_norm,
-                                       normalizer_params=normalizer_params)
-        lx_z = layers.conv2d_transpose(lx_z, 3, 5, stride=2,
-                                       activation_fn=tf.nn.sigmoid)
+        lx_z = tf.layers.dense(z, ngf * 8 * 4 * 4, use_bias=False)
+        lx_z = tf.layers.batch_normalization(lx_z, training=is_training)
+        lx_z = tf.nn.relu(lx_z)
+        lx_z = tf.reshape(lx_z, [-1, 4, 4, ngf * 8])
+        lx_z = tf.layers.conv2d_transpose(lx_z, ngf * 4, 5, strides=(2, 2),
+                                          padding='same', use_bias=False)
+        lx_z = tf.layers.batch_normalization(lx_z, training=is_training)
+        lx_z = tf.nn.relu(lx_z)
+        lx_z = tf.layers.conv2d_transpose(lx_z, ngf * 2, 5, strides=(2, 2),
+                                          padding='same', use_bias=False)
+        lx_z = tf.layers.batch_normalization(lx_z, training=is_training)
+        lx_z = tf.nn.relu(lx_z)
+        lx_z = tf.layers.conv2d_transpose(lx_z, 3, 5, strides=(2, 2),
+                                          padding='same', activation=tf.sigmoid)
     return generator, lx_z
 
 
 @zs.reuse('discriminator')
 def discriminator(x, is_training):
-    normalizer_params = {'is_training': is_training,
-                         'updates_collections': None}
     ndf = 32
-    lc_x = layers.conv2d(x, ndf*2, 5, stride=2,
-                         normalizer_fn=layers.batch_norm,
-                         normalizer_params=normalizer_params)
-    lc_x = layers.conv2d(lc_x, ndf*4, 5, stride=2,
-                         normalizer_fn=layers.batch_norm,
-                         normalizer_params=normalizer_params)
-    lc_x = layers.conv2d(lc_x, ndf*8, 5, stride=2,
-                         normalizer_fn=layers.batch_norm,
-                         normalizer_params=normalizer_params)
-    lc_x = layers.flatten(lc_x)
-    class_logits = layers.fully_connected(lc_x, 1, activation_fn=None)
+    lc_x = tf.layers.conv2d(x, ndf * 2, 5, strides=(2, 2),
+                            padding='same', use_bias=False)
+    lc_x = tf.layers.batch_normalization(lc_x, training=is_training)
+    lc_x = tf.nn.relu(lc_x)
+    lc_x = tf.layers.conv2d(lc_x, ndf * 4, 5, strides=(2, 2),
+                            padding='same', use_bias=False)
+    lc_x = tf.layers.batch_normalization(lc_x, training=is_training)
+    lc_x = tf.nn.relu(lc_x)
+    lc_x = tf.layers.conv2d(lc_x, ndf * 8, 5, strides=(2, 2),
+                            padding='same', use_bias=False)
+    lc_x = tf.layers.batch_normalization(lc_x, training=is_training)
+    lc_x = tf.nn.relu(lc_x)
+    lc_x = tf.reshape(lc_x, [-1, ndf * 8 * 4 * 4])
+    class_logits = tf.layers.dense(lc_x, 1)
     return class_logits
 
 
-if __name__ == "__main__":
+def main():
     tf.set_random_seed(1234)
+    np.random.seed(1234)
 
     # Load CIFAR
     data_path = os.path.join(conf.data_dir, 'cifar10',
                              'cifar-10-python.tar.gz')
-    np.random.seed(1234)
     x_train, t_train, x_test, t_test = \
         dataset.load_cifar10(data_path, normalize=True, one_hot=True)
     _, n_xl, _, n_channels = x_train.shape
@@ -77,24 +77,18 @@ if __name__ == "__main__":
     n_z = 40
 
     # Define training/evaluation parameters
-    lb_samples = 1
     epochs = 1000
     batch_size = 32 * FLAGS.num_gpus
     gen_size = 100
     iters = x_train.shape[0] // batch_size
     print_freq = 100
-    test_freq = iters
-    save_freq = iters
-    learning_rate = 0.0002
-    anneal_lr_freq = 200
-    anneal_lr_rate = 0.75
+    save_freq = 100
 
     # Build the computation graph
     is_training = tf.placeholder(tf.bool, shape=[], name='is_training')
     x = tf.placeholder(tf.float32, shape=(None, n_xl, n_xl, n_channels),
                        name='x')
-    learning_rate_ph = tf.placeholder(tf.float32, shape=[], name='lr')
-    optimizer = tf.train.AdamOptimizer(learning_rate_ph, beta1=0.5)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5)
 
     def build_tower_graph(x, id_):
         tower_x = x[id_ * tf.shape(x)[0] // FLAGS.num_gpus:
@@ -137,19 +131,19 @@ if __name__ == "__main__":
                 tower_grads.append(grads)
     gen_loss, disc_loss = multi_gpu.average_losses(tower_losses)
     grads = multi_gpu.average_gradients(tower_grads)
-    infer = optimizer.apply_gradients(grads)
-    _, eval_x_gen = generator(None, gen_size, n_z, is_training)
 
-    gen_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
-                                     scope='generator')
-    saver = tf.train.Saver(max_to_keep=10, var_list=gen_var_list)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        infer_op = optimizer.apply_gradients(grads)
+
+    # Generate images
+    _, eval_x_gen = generator(None, gen_size, n_z, False)
 
     # Run the inference
     with multi_gpu.create_session() as sess:
         sess.run(tf.global_variables_initializer())
+
         for epoch in range(1, epochs + 1):
-            if epoch % anneal_lr_freq == 0:
-                learning_rate *= anneal_lr_rate
             np.random.shuffle(x_train)
             gen_losses, disc_losses = [], []
             time_train = -time.time()
@@ -157,10 +151,8 @@ if __name__ == "__main__":
                 iter = t + 1
                 x_batch = x_train[t * batch_size:(t + 1) * batch_size]
                 _, g_loss, d_loss = sess.run(
-                    [infer, gen_loss, disc_loss],
-                    feed_dict={x: x_batch,
-                               learning_rate_ph: learning_rate,
-                               is_training: True})
+                    [infer_op, gen_loss, disc_loss],
+                    feed_dict={x: x_batch, is_training: True})
                 gen_losses.append(g_loss)
                 disc_losses.append(d_loss)
 
@@ -173,19 +165,15 @@ if __name__ == "__main__":
                     gen_losses = []
                     disc_losses = []
 
-                if iter % test_freq == 0:
-                    time_test = -time.time()
-                    images = sess.run(eval_x_gen,
-                                      feed_dict={is_training: False})
+                if iter % save_freq == 0:
+                    images = sess.run(eval_x_gen)
                     name = "results/dcgan/dcgan.epoch.{}.iter.{}.png".format(
                         epoch, iter)
                     save_image_collections(images, name, scale_each=True)
-                    time_test += time.time()
-
-                if iter % save_freq == 0:
-                    save_path = "results/dcgan/dcgan.epoch.{}.iter.{}.ckpt". \
-                        format(epoch, iter)
-                    saver.save(sess, save_path)
 
                 if iter % print_freq == 0:
                     time_train = -time.time()
+
+
+if __name__ == "__main__":
+    main()
